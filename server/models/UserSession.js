90d1e7fe2318e4e5fdcf436b9e5d31e27d5b37b3
kavinda-seasath <seasath@outlook.com>
2018-06-23 18:21:20 +0530

    created the API configs

---
+++ server/models/UserSession.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const UserSessionSchema = new mongoose.Schema({
+
+  userId: {
+    type:String,
+    default: ''
+  },
+  timestamp: {
+    type:Date,
+    default: Date.now()
+  },
+  isDeleted: {
+    type:Boolean,
+    default: false
+  },
+  username: {
+    type:String,
+    default: ''
+  }
+
+
+});
+
+module.exports = mongoose.model('UserSession', UserSessionSchema);
